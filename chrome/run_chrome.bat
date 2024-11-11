523c509203105d762cbcb555f33e09330f00fb54
uxumax <uxumax@gmail.com>
2024-11-11 10:51:39 -0500

    clear debug garbage

--- chrome/run_chrome.bat
+++
@@ -1,7 +0,0 @@
-REM @echo off
-REM set CHROMIUM_VERSION=127.0.6478.127
-
-Z:\home\wineuser\chrome\bin\chrome.exe --no-sandbox
-REM ^ Z:\home\wineuser\chrome\bin\chrome.exe ^
-REM ^ --user-data-dir="Z:\home\wineuser\chrome\profiles\profile1" ^
-REM ^ --no-sandbox ^
